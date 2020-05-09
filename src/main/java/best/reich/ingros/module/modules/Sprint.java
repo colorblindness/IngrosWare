e65dbaebb92794e30b9fc39dbecf3e9e5a099c48
Zim The Destroyer <gamingsoutherner@gmail.com>
2020-05-09 12:15:39 -0500

    Revert "I am not pushing this"
    This reverts commit 116e57762d0ef10d6a658a715c97a4c790de2a18.

---
+++ src/main/java/best/reich/ingros/module/modules/Sprint.java
@@ -0,0 +1,33 @@
+package best.reich.ingros.module.modules;
+
+import best.reich.ingros.events.entity.UpdateEvent;
+import net.b0at.api.event.types.EventType;
+import me.xenforu.kelo.module.ModuleCategory;
+import me.xenforu.kelo.module.annotation.ModuleManifest;
+import me.xenforu.kelo.module.type.ToggleableModule;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.init.MobEffects;
+import net.b0at.api.event.Subscribe;
+
+/**
+ * made by Xen for IngrosWare
+ * at 1/3/2020
+ **/
+@ModuleManifest(label = "Sprint", category = ModuleCategory.MOVEMENT, color = 0xff00ff00)
+public class Sprint extends ToggleableModule {
+
+    @Subscribe
+    public void onUpdate(UpdateEvent event) {
+        if(event.getType() == EventType.PRE) {
+            if (isSprintable(mc.player))
+                mc.player.setSprinting(true);
+        }
+    }
+
+    private boolean isSprintable(EntityPlayerSP player) {
+        return !player.isSprinting() && !player.isSneaking() && player.movementInput.moveForward > 0.0f &&
+                (player.getFoodStats().getFoodLevel() >= 6f || player.capabilities.allowFlying) &&
+                !player.isPotionActive(MobEffects.BLINDNESS);
+    }
+
+}
